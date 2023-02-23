1bfdc5b5e49637c1485a623773380bcc4f904952
Leonardo Ribeiro <leoribeiro.dsgn@gmail.com>
2023-02-23 20:27:43 -0300

    added sidebar

--- src/layouts/BaseTemplate/BaseTemplate.tsx
+++ src/layouts/BaseTemplate/BaseTemplate.tsx
@@ -9,6 +9,13 @@ interface BaseTemplateProps {
 export const BaseTemplate: FC<BaseTemplateProps> = props => {
   return (
     <div className="min-h-screen w-full bg-background-color text-white">
+      <div
+        className={
+          'fixed left-0 top-0 z-20 flex h-full w-[80px] flex-col items-center border-r-[1px] border-r-gray-500 bg-background-color p-4'
+        }
+      >
+        lal
+      </div>
       <MainNavigation />
       <div className={'py-28 px-10 sm:px-0'}>
         <Container>{props.children}</Container>
