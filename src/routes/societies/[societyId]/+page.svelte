43d05542af29959198460ebba859b4b30e2c3fd7
evok3d <evok3d22@gmail.com>
2024-11-05 16:15:05 -0300

    Update +page.svelte

--- src/routes/societies/[societyId]/+page.svelte
+++ src/routes/societies/[societyId]/+page.svelte
@@ -26,7 +26,7 @@
 		</div>
 		<div class="w-full flex gap-8 mt-8">
 			<div class="w-1/3">
-				<section class="w-full p-8 rounded-xl relative" style="background: #1b1f23;">
+				<section class="w-full p-8 rounded-xl relative" style="background: #fff;">
 					{#if data.canEditSociety}
 						<DotsMenu
 							editHref={$page.url.pathname + '/edit'}
@@ -103,7 +103,7 @@
 												onDelete={() => console.log('delete')}
 											/>
 										{/if}
-										<div class="flex w-full gap-6 p-4 rounded-xl" style="background: #1b1f23;">
+										<div class="flex w-full gap-6 p-4 rounded-xl" style="background: #fff;">
 											<div>
 												{#if data.courses[id].image}
 													<img
