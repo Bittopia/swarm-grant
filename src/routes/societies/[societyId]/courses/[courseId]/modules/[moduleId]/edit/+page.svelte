8ee384cc42e596549ba995006c070d833e99f6a7
evok3d <evok3d22@gmail.com>
2024-11-07 21:32:19 -0300

    Update +page.svelte

--- src/routes/societies/[societyId]/courses/[courseId]/modules/[moduleId]/edit/+page.svelte
+++ src/routes/societies/[societyId]/courses/[courseId]/modules/[moduleId]/edit/+page.svelte
@@ -102,10 +102,7 @@
 					<Input type="hidden" name="id" value={moduleId} />
 					<Input type="hidden" name="societyId" value={societyId} />
 					<Input type="hidden" name="courseId" value={courseId} />
-					<div
-						class="w-full p-4 rounded-xl grid gap-6 mb-6 md:grid-cols-1"
-						style="border: 1px solid #424148"
-					>
+					<div class="mt-8 p-8 rounded-xl grid gap-6 mb-6 md:grid-cols-1" id="module" style="background: #fff;">
 						<div>
 							<Label for="image" class="mb-2">What's the module image?</Label>
 							<Fileupload
