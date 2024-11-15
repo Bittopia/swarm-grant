ca17b1ffafe997b30eaf347343e52c422cb8ab1a
evok3d <evok3d22@gmail.com>
2024-11-15 00:10:21 -0300

    Update SocietyBox.svelte

--- src/lib/components/SocietyBox/SocietyBox.svelte
+++ src/lib/components/SocietyBox/SocietyBox.svelte
@@ -11,7 +11,7 @@
 			<img
 				src={society.image + '?img-format=webp'}
 				alt="Society banner"
-				class="w-full h-72 object-cover bg-gray-100 dark:bg-gray-600 rounded-t-lg mt-3"
+				class="w-full aspect-[4/3] object-cover bg-gray-100 dark:bg-gray-600 rounded-t-lg mt-3"
 			/>
 
 		{:else}
