181f033406d42d97c9ecd1e8d6c0c0d51d46cffa
evok3d <evok3d22@gmail.com>
2024-11-20 01:18:38 -0300

    Update FormSpinner.svelte

--- src/lib/components/FormSpinner.svelte
+++ src/lib/components/FormSpinner.svelte
@@ -5,13 +5,14 @@
 </script>
 
 <div class="absolute top-0 bottom-0 right-0 left-0">
-	<div class="flex flex-col gap-4 items-center justify-center h-full bg-gray-900/40">
+	<div class="flex flex-col gap-4 items-center justify-center h-full bg-[rgb(35,0,138)] text-white dark:text-white">
 		<Spinner size="12" />
 
 		{#if phrase}
-			<span class="mt-8 text-sm dark:text-white [text-shadow:_0_1px_0_rgb(0_0_0_/_80%)]"
-				>{phrase}</span
-			>
+			<span class="mt-8 text-sm text-white dark:text-white [text-shadow:_0_1px_0_rgb(0_0_0_/_80%)]">
+				{phrase}
+			</span>
 		{/if}
 	</div>
 </div>
+
