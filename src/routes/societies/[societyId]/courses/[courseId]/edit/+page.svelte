4632d8e5bb937275e3824abf1eee370ac1f0da0c
evok3d <evok3d22@gmail.com>
2024-11-19 23:10:49 -0300

    Update +page.svelte

--- src/routes/societies/[societyId]/courses/[courseId]/edit/+page.svelte
+++ src/routes/societies/[societyId]/courses/[courseId]/edit/+page.svelte
@@ -28,7 +28,7 @@
 <Container>
 	<div class="w-full mb-4">
 		<div class="flex items-center justify-between">
-			<div class="flex items-center gap-4">
+			<div class="flex items-center gap-4 mb-5">
 				<BackButton href={returnTo || `/societies/${societyId}/courses/${courseId}`} />
 			</div>
 		</div>
@@ -60,6 +60,57 @@
 					<Input type="hidden" name="id" value={courseId} />
 					<Input type="hidden" name="societyId" value={societyId} />
 
+
+					<div>
+						<Label for="name" class="mt-3 mb-4 text-lg">Name Your Course</Label>
+						<Input
+							name="name"
+							disabled={requesting}
+							type="text"
+							id="name"
+							placeholder="Eg.: Linear algebra basics"
+							value={$page.data.course.name}
+							required
+						/>
+					</div>
+					<div>
+						<Label for="description" class="mt-3 mb-4 text-lg">Explain What Makes Your Course Unique and Impactful</Label>
+						<Textarea
+							class="resize-none"
+							rows="8"
+							name="description"
+							disabled={requesting}
+							type="text"
+							id="description"
+							placeholder="Provide a brief overview of your course content and goals..."
+							value={$page.data.course.description}
+							required
+						/>
+					</div>
+					<div>
+						<Label for="startDate" class="mt-3 mb-4 text-lg">Choose a Date to Launch Your Course</Label>
+						<Datepicker
+							name="startDate"
+							datepickerOrientation="top"
+							datepickerFormat="dd/mm/yyyy"
+							datepickerTitle=""
+							placeholder="dd/mm/yyyy"
+							value={$page.data.course.startDate}
+							required
+						/>
+					</div>
+					<div>
+						<Label for="description" class="mt-3 mb-4 text-lg">Provide the Wallet Address for the Course Educator</Label>
+						<Input
+							id="wallet-address"
+							name="educator"
+							disabled={requesting}
+							type="text"
+							placeholder="Wallet address"
+							value={$page.data.course.educator}
+							required
+						/>
+					</div>
 					<div class="flex items-center gap-4">
 						{#if files && files.length}
 							<img
@@ -78,63 +129,14 @@
 							<Label for="image" class="mb-2">What's the course image?</Label>
 							<Fileupload
 								name="image"
+								class="image-button"
+								id="upload-button"
 								disabled={requesting}
 								accept="image/*"
 								on:change={(e) => (files = e.target.files)}
 							/>
 						</div>
 					</div>
-					<div>
-						<Label for="name" class="mb-2">What's the course name?</Label>
-						<Input
-							name="name"
-							disabled={requesting}
-							type="text"
-							id="name"
-							placeholder="Eg.: Linear algebra basics"
-							value={$page.data.course.name}
-							required
-						/>
-					</div>
-					<div>
-						<Label for="description" class="mb-2">Give a description for your course.</Label>
-						<Textarea
-							class="resize-none"
-							rows="8"
-							name="description"
-							disabled={requesting}
-							type="text"
-							id="description"
-							placeholder="Write a description about your course"
-							value={$page.data.course.description}
-							required
-						/>
-					</div>
-					<div>
-						<Label for="startDate" class="mb-2">Course date</Label>
-						<Datepicker
-							name="startDate"
-							datepickerOrientation="top"
-							datepickerFormat="dd/mm/yyyy"
-							datepickerTitle=""
-							placeholder="dd/mm/yyyy"
-							value={$page.data.course.startDate}
-							required
-						/>
-					</div>
-					<div>
-						<Label for="description" class="mb-2">Educator wallet address</Label>
-						<Input
-							id="wallet-address"
-							name="educator"
-							disabled={requesting}
-							type="text"
-							placeholder="Wallet address"
-							value={$page.data.course.educator}
-							required
-						/>
-					</div>
-
 					{#if form?.error}
 						<Alert color="red">
 							{form.error}
@@ -147,7 +149,7 @@
 							disabled={requesting}
 							text={returnTo ? 'Back to courses list' : 'Back to course'}
 						/>
-						<Button disabled={requesting} type="submit" class="rounded-full px-8">Submit</Button>
+						<Button disabled={requesting} type="submit" class="rounded-full px-8 cta">Update</Button>
 					</div>
 				</div>
 			</form>
