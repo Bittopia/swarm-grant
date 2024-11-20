b1a09e00885a29cc9afde9fdfd64cbe4d51ca0a5
evok3d <evok3d22@gmail.com>
2024-11-20 01:41:55 -0300

    Update FormSpinner.svelte

--- src/lib/components/FormSpinner.svelte
+++ src/lib/components/FormSpinner.svelte
@@ -4,7 +4,7 @@
 	export let phrase: string;
 </script>
 
-<div class="absolute top-0 bottom-0 right-0 left-0">
+<div class="absolute top-0 bottom-0 right-0 left-0 z-50">
 	<div class="flex flex-col gap-4 items-center justify-center h-full bg-[rgb(35,0,138)] text-white dark:text-white">
 		<Spinner size="12" />
 
